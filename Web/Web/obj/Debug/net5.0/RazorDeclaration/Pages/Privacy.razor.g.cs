71a226280228372980158b81e2beca85a8f27c22
WutenderBorsch <anastasia.terr@mail.ru>
2021-05-31 01:08:44 +0300

    Edit

--- Web/Web/obj/Debug/net5.0/RazorDeclaration/Pages/Privacy.razor.g.cs
+++ Web/Web/obj/Debug/net5.0/RazorDeclaration/Pages/Privacy.razor.g.cs
@@ -4,7 +4,7 @@
 #pragma warning disable 0649
 #pragma warning disable 0169
 
-namespace Web.Shared
+namespace Web.Pages
 {
     #line hidden
     using System;
@@ -82,29 +82,14 @@ using Web.Shared;
 #line default
 #line hidden
 #nullable disable
-    public partial class NavMenu : Microsoft.AspNetCore.Components.ComponentBase
+    [Microsoft.AspNetCore.Components.RouteAttribute("/Privacy")]
+    public partial class Privacy : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
         }
         #pragma warning restore 1998
-#nullable restore
-#line 32 "C:\Users\anast\Documents\GitHub\ComplexTask\Web\Web\Shared\NavMenu.razor"
-       
-    private bool collapseNavMenu = true;
-
-    private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
-
-    private void ToggleNavMenu()
-    {
-        collapseNavMenu = !collapseNavMenu;
-    }
-
-
-#line default
-#line hidden
-#nullable disable
     }
 }
 #pragma warning restore 1591
